be06ab2ebc8c5d7d8fcb5df6444ccbb15a637b26
Prasad Gondake <prasadgondake0910@gmail.com>
2023-08-23 23:52:02 +0530

    Initial commit

---
+++ Frontend/build/precache-manifest.8d195e76adcf2c8f91327ef8297f5d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ccf125c911486b624e7",
+    "url": "/static/css/main.4d6424f7.chunk.css"
+  },
+  {
+    "revision": "0ccf125c911486b624e7",
+    "url": "/static/js/main.d1bb7504.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6923333910f388d8959b",
+    "url": "/static/js/2.075d17b9.chunk.js"
+  },
+  {
+    "revision": "1af68cbeb63f40d4262f43779e113161",
+    "url": "/static/media/spinner.1af68cbe.gif"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "f96ceefd639d1b76bee3b50b7b2d467a",
+    "url": "/index.html"
+  }
+];
